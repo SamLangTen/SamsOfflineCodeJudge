ce7b3ae639375e57a63e977932cc680abd6c4b42
SamLangTen <samlangten@outlook.com>
2016-06-20 09:02:37 +0800

    add: basic language pair

--- SamsOfflineCodeJudge/JudgerManager.cs
+++ SamsOfflineCodeJudge/JudgerManager.cs
@@ -14,8 +14,17 @@ namespace SamsOfflineCodeJudge
     }
     public class JudgerManager
     {
+        static JudgerManager()
+        {
+            LanguageExtensions = new Dictionary<string, string>();
+            LanguageExtensions.Add("C", "c");
+            LanguageExtensions.Add("C++", "cpp");
+            LanguageExtensions.Add("VB.Net", "vb");
+            LanguageExtensions.Add("C#", "cs");
+            LanguageExtensions.Add("Pascal", "pas");
+        }
         public static SystemUser RunningUser { get; set; }
         public static int MaximumTime { get; set; } = 20000;
-        public static Dictionary<string, string> LanguageExtensions = new Dictionary<string, string>();
+        public static Dictionary<string, string> LanguageExtensions { get; set; }
     }
 }
