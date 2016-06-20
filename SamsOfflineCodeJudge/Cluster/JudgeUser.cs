e020c32a81e3ebf3b5f3b1b446e19edc8844719c
SamLangTen <samlangten@outlook.com>
2016-06-20 11:10:52 +0800

    update: change files locations

---
+++ SamsOfflineCodeJudge/Cluster/JudgeUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamsOfflineCodeJudge
+{
+    public class JudgeUser
+    {
+        public string Id { get; set; }
+        public List<JudgeUnit> Units { get; set; } = new List<JudgeUnit>();
+    }
+}
---
+++ SamsOfflineCodeJudge/Cluster/JudgerCluster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamsOfflineCodeJudge
+{
+    public class JudgerCluster
+    {
+        public List<JudgeUnit> JudgeUnits { get; set; } = new List<JudgeUnit>();
+        public List<SampleData> SampleDatas { get; set; } = new List<SampleData>();
+
+    }
+}
